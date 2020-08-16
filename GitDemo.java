cb24a135c9e32f6903dc565c4b3e7b5348782d37
EkkoYu <17712427671@163.com>
2020-08-16 12:28:12 +0800

    4.测试本地向远程仓库提交

--- GitDemo.java
+++ GitDemo.java
@@ -11,6 +11,7 @@ public class GitDemo {
      System.out.println("Hello Git");
      System.out.println("2.测试文件修改");
      System.out.println("3.测试版本回退");
+     System.out.println("4.测试本地向远程仓库提交");
      
   }
   
