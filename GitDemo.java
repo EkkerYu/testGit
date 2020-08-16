b217da8916503f09971bccaa81dc5e4639f95080
EkkerYu <49644936+EkkerYu@users.noreply.github.com>
2020-08-16 12:38:01 +0800

    Update GitDemo.java

--- GitDemo.java
+++ GitDemo.java
@@ -12,6 +12,7 @@ public class GitDemo {
      System.out.println("2.测试文件修改");
      System.out.println("3.测试版本回退");
      System.out.println("4.测试本地向远程仓库提交");
+     System.out.println("5.测试拉取远程仓库的修改");
      
   }
   
