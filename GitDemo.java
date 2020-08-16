c203cd5b046b04a9765b67aca736271696255a21
EkkoYu <17712427671@163.com>
2020-08-16 14:21:21 +0800

    7.测试冲突解决,这是dev分支添加的

--- GitDemo.java
+++ GitDemo.java
@@ -13,7 +13,7 @@ public class GitDemo {
      System.out.println("3.测试版本回退");
      System.out.println("4.测试本地向远程仓库提交");
      System.out.println("5.测试拉取远程仓库的修改");
-     System.out.println("5.测试在dev分支上的修改");
+     System.out.println("6.测试在dev分支上的修改");
      
   }
   
